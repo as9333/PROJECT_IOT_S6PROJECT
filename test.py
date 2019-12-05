bf117f746cd3194cb7fe618e158d0dfef139fe6e
Ashok Manoj Philip <ashokmanojphilip@gmail.com>
2019-12-05 19:37:42 +0530

    Update test.py

--- test.py
+++ test.py
@@ -3,8 +3,22 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-    return 'OK!'
+def login():
+
+	# if 'submit' in request.form:
+	# 	user=request.form['username']
+	# 	pas=request.form['password']
+
+	# 	q="select * from login where username='%s' and password='%s'"%(user,pas)
+	# 	res=select(q)
+
+	# 	if res:
+	# 		if res[0]['usertype']=="admin":
+	# 			return redirect(url_for("admin.home"))
+	# 		elif res[0]['usertype']=="user":
+	# 			return redirect(url_for("public.home"))
+
+	return render_template("login.html")
 
 if __name__=="__main__":
     app.run()
