68f3342a624b8a43dce2549fd33fb6acd7b48ac3
Ashok Manoj Philip <ashokmanojphilip@gmail.com>
2020-01-02 02:56:04 +0530

    UPDATE
    Can save automatic jobs on sql and delete them

--- admin.py
+++ admin.py
@@ -1,5 +1,5 @@
 from flask import*
-# from database import*
+from database import*
 import uuid
 
 
--- public.py
+++ public.py
@@ -4,14 +4,22 @@ import uuid
 import requests
 import schedule  #pip install schedule
 import time
+import pymysql #pip install PyMySQL
+import mysql.connector
 
 public=Blueprint('public',__name__)
 
+mydatabase = mysql.connector.connect(
+    host = 'localhost', user = 'root',
+    passwd = '', database = 'smart_switch')
+
+mycursor = mydatabase.cursor()
+
 # @public.route('/jsfunctions',methods=['post','get'])
-def pin_on():
-	print("Entered fn pin_on")
-	# return render_template('jsfunctions.html')
-	return "Entered fn pin_on"
+# def pin_on():
+# 	print("Entered fn pin_on")
+# 	# return render_template('jsfunctions.html')
+# 	return "Entered fn pin_on"
 
 
 @public.route('/login',methods=['post','get'])
@@ -101,7 +109,7 @@ def registration():
 
 @public.route('/automatic_on_off',methods=['post','get'])
 def automatic_on_off():
-
+	# data={}
 	# if not session.get('logged_in'):
 	# 	return redirect(url_for("public.login"))
 	# else:	
@@ -110,17 +118,57 @@ def automatic_on_off():
 	if 'submit' in request.form:
 		from_time=request.form['from_time']	
 		to_time=request.form['to_time']
-		button=request.form['button']
+		relay=request.form['button']
 		status=request.form['status']
-		# print("from_time", file=sys.stderr)
-		# print('enter getJSONReuslt', flush=True)
+
+		if relay == 'D5':                      #for seleting relay and also outputing the exixting jobs
+			button='button 1'
+		if relay == 'D4':
+			button='button 2'
+		if relay == 'D0':
+			button='button 3'
+		if relay == 'D2':
+			button='button 4'
+		
 		print("From_time=",from_time)
 		print("to_time=",to_time)
 		print("button=",button)
 		print("status=",status)
-		pin_on()
+		# pin_on()
+
+		q="INSERT INTO `automatic_jobs` VALUES (0,'%s','%s','%s','%s','%s')"%(from_time,to_time,button,status,relay)
+		id=insert(q)
+		print("id=",id)
+
+	# host='localhost'
+    # user=
+    # password=''
+    # db='smart_switch'	
+	# con=pymysql.connect(host=host,user='root',password='',db='smart_switch', use_unicode=True, charset='utf8')
+
 
 	if not session.get('logged_in'):
 		return redirect(url_for("public.login"))
 	else:	
-		return render_template('automatic_on_off.html')
+		mycursor.execute('SELECT * FROM automatic_jobs')
+		data=mycursor.fetchall()
+		# cur = con.cursor()
+		# cur.execute("SELECT * FROM automatic_jobs")
+		# data = cur.fetchall()
+		return render_template('automatic_on_off.html',output_data=data)
+
+@public.route('/delete_jobs',methods=['post','get'])
+def delete_jobs():
+
+	if 'submit' in request.form:
+		job_id=request.form['job_id']
+		print("job_id to delete=",job_id)	
+		query="DELETE FROM `automatic_jobs` WHERE `automatic_jobs`.`JOB_ID` = %s"
+		mycursor.execute(query,(job_id,))
+		
+	if not session.get('logged_in'):
+		return redirect(url_for("public.login"))
+	else:	
+		mycursor.execute('SELECT * FROM automatic_jobs')
+		data=mycursor.fetchall()	
+		return render_template('delete_jobs.html',output_data=data)
